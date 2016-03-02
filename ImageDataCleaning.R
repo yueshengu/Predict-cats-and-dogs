78c1e73f8a72d779184be6b174e19a5b0a8c34a1
Yue Shen Gu <yueshengu14@gmail.com>
2016-03-02 17:20:11 -0500

    update

--- ImageDataCleaning.R
+++ ImageDataCleaning.R
@@ -20,3 +20,24 @@ display(Image.fHigh[,,3])
 
 moments = computeFeatures.moment(Image.fHigh[,,2])
 ftp = computeFeatures.shape(Image.fHigh[,,2])
+
+
+
+par(mar=c(0,0,0,0), xpd=NA, mgp=c(0,0,0), oma=c(0,0,0,0), ann=F)
+#par(mar=c(5, 4, 4, 2) + 0.1)
+plot.new()
+plot.window(c(1,600),c(1,400))
+usr<-par("usr")    
+rasterImage(Image.fHigh[,,3], usr[1], usr[3], usr[2], usr[4])
+#text(.1,.1,'a',col='red',cex=4)
+sapply(1:nrow(moments),function(i){
+  text((moments[i,1]),(400-moments[i,2]),i,cex=1,col='red')
+})
+moments2<-moments[moments[,3]>=34,]
+sapply(1:nrow(moments2),function(i){
+  text((moments2[i,1]),(400-moments2[i,2]),i,cex=1,col='blue')
+})
+moments3<-moments[moments[,3]<34,]
+sapply(1:nrow(moments3),function(i){
+  text((moments3[i,1]),(400-moments3[i,2]),i,cex=1,col='green')
+})
